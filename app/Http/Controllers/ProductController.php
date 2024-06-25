73575056b9e502de1bf5143937e876b7abd2a50f
Shandy Dailami <shandydailami@gmail.com>
2024-06-25 11:50:16 +0800

    First Commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+
+use Illuminate\Http\Request;
+
+use Illuminate\View\View;
+
+class ProductController extends Controller
+{
+    public function index(): View
+    {
+        $products = Product::latest()->paginate(10);
+
+        return view('products.index', compact('products'));
+    }
+}
